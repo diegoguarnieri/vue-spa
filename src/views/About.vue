649f4b64300d8fbadb4c1fe6fe26af2aa80844ca
Diego <diego.guarnieri@gmail.com>
2021-09-01 17:35:57 -0300

    Vue SPA auth

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,26 +3,69 @@
         <h1>About</h1>
     </div>
 
+    <button @click="test()">test</button>
+
 </template>
 
 <script>
+import axios from 'axios'
+axios.defaults.withCredentials = true
+//axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+//axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
 export default {
     data() {
         return {
-            
+            endpoint: 'https://api.guarnieri.ca'            
         }
     },
     mounted() {
         this.getData()
     },
     methods: {
+        test: function() {
+            axios({
+                method: 'get',
+                url: this.endpoint + '/api/test'
+            })
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+            .finally(() => {
+                
+            })
+        },
         login: function() {
+            //axios.defaults.withCredentials = true
 
+            let data = {
+                email: 'diego@test.com',
+                password: 'diego'
+            }
+
+            axios({
+                method: 'post',
+                url: this.endpoint + '/api/login',
+                data: data
+            })
+            .then(response => {
+                console.log(response)
+                this.test()
+            })
+            .catch(error => {
+                console.log(error)
+            })
+            .finally(() => {
+                
+            })
         },
         getData: function() {
-            this.axios({
+            axios({
                 method: 'get',
-                url: 'http://172.16.4.160:9040/sanctum/csrf-cookie'
+                url: this.endpoint + '/sanctum/csrf-cookie'
             })
             .then(response => {
                 console.log(response)
