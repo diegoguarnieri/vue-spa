ac8d9d7308d3f87138d21848a4cd0ee30922b480
Diego <diego.guarnieri@gmail.com>
2021-09-06 09:33:23 -0300

    Coreui menu

--- src/main.js
+++ src/main.js
@@ -4,13 +4,14 @@ import router from './router'
 import axios from 'axios'
 import coreui from '@coreui/coreui/dist/js/coreui.min.js'
 
-//import "bootstrap/dist/css/bootstrap.min.css"
-//import "bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import '@coreui/coreui/dist/css/coreui.min.css'
 import '@coreui/coreui'
 
-import "@/assets/scss/style.scss"
+import '@/assets/scss/style.scss'
 
 const app = createApp(App)
 
